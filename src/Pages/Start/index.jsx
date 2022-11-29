5414e851f4cb3685a05de7ebb5f4fd5a1e03f27f
Rafael Henrique da Silva Pereira <henrique014@gmail.com>
2022-11-29 18:03:45 -0300

    Inserindo Botão Padrao com os estilos

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,9 +1,15 @@
 import React, {useEffect} from "react";
 import {View, Text, StyleSheet, Image, ScrollView} from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
+import DefaultButton from "../../Components/Common/DefaultButton";
 import LifeStatus from "../../Components/Common/LifeStatus";
 
 export default function Start() {
+    const handleNavAppExplanation = () => {
+        console.log("Testando o clique")
+    }
+
     return (
         <View style={styles.container}>
             <ScrollView style={styles.scroll} showsVerticalScrollIndicator={false}>
@@ -16,6 +22,12 @@ export default function Start() {
                     <Text style={styles.description}>
                     Vamos transformar sua vida {"\n"} em um jogo, buscando sempre {"\n"} o melhor nível.
                     </Text>
+                    <DefaultButton
+                        buttonText={"Continuar"}
+                        handlePress={handleNavAppExplanation}
+                        width={250}
+                        height={50}
+                    />
                 </View>
             </ScrollView>
         </View>
