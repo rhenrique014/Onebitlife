8017ebd074a71aef764f42c2c02de3c5365c2053
Rafael Henrique da Silva Pereira <henrique014@gmail.com>
2022-11-30 15:30:28 -0300

    Barra de Status

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -3,6 +3,7 @@ import { useNavigation } from "@react-navigation/native";
 import {View, Text, StyleSheet, ScrollView} from "react-native";
 
 import LifeStatus from "../../Components/Common/LifeStatus";
+import StatusBar from "../../Components/Home/StatusBar";
 
 export default function Home() {
     const navigation = useNavigation();
@@ -18,6 +19,7 @@ export default function Home() {
                     <Text style={styles.dailyChecks}>❤️ 20 dias - ✔️ 80 checks</Text>
 
                     <LifeStatus />
+                    <StatusBar />
                 </View>
                 <Text
                     style={styles.explanationText}
