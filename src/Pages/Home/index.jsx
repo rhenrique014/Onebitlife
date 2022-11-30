cf4491584db41123dfa3d67759fdfd59d530ce78
Rafael Henrique da Silva Pereira <henrique014@gmail.com>
2022-11-30 16:29:30 -0300

    Edição e Criação de Hánitos

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -5,6 +5,7 @@ import {View, Text, StyleSheet, ScrollView} from "react-native";
 import LifeStatus from "../../Components/Common/LifeStatus";
 import StatusBar from "../../Components/Home/StatusBar";
 import CreateHabit from "../../Components/Home/CreateHabit/indes";
+import EditHabit from "../../Components/Home/EditHabit";
 
 export default function Home() {
     const navigation = useNavigation();
@@ -26,18 +27,49 @@ export default function Home() {
                     <LifeStatus />
                     <StatusBar />
 
-                    <CreateHabit habitArea="Mente" borderColor="#90B7F3" />
+                    {mindHabit ? (
+                        <EditHabit
+                        habit={mindHabit?.habitName}
+                        frequency={'${mindHabit?.habitTime} - ${mindHabit?.habitFrequency}'}
+                        habitArea={mindHabit?.habitArea}
+                        checkColor="#90B7F3"
+                        />
+                    ) : (
+                        <CreateHabit habitArea="Mente" borderColor="#90B7F3" />
+                    )}
 
-                    <CreateHabit habitArea="Financeiro" borderColor="#85BB65" />
+                    {moneyHabit ? (
+                        <EditHabit
+                        habit={moneyHabit?.habitName}
+                        frequency={'${moneyHabit?.habitTime} - ${moneyHabit?.habitFrequency}'}
+                        habitArea={moneyHabit?.habitArea}
+                        checkColor="#85BB65"
+                        />
+                    ) : (
+                        <CreateHabit habitArea="Financeiro" borderColor="#85BB65" />
+                    )}
 
-                    <CreateHabit habitArea="Corpo" borderColor="#FF0044" />
+                    {bodyHabit ? (
+                        <EditHabit
+                        habit={bodyHabit?.habitName}
+                        frequency={'${bodyHabit?.habitTime} - ${bodyHabit?.habitFrequency}'}
+                        habitArea={bodyHabit?.habitArea}
+                        checkColor="#FF0044"
+                        />
+                    ) : (
+                        <CreateHabit habitArea="Corpo" borderColor="#FF0044" />
+                    )}
 
-                    <CreateHabit habitArea="Humor" borderColor="#FE7F23" />
-
-
-
-
-
+                    {funHabit ? (
+                        <EditHabit
+                        habit={funHabit?.habitName}
+                        frequency={'${funHabit?.habitTime} - ${funHabit?.habitFrequency}'}
+                        habitArea={funHabit?.habitArea}
+                        checkColor="#FE7F23"
+                        />
+                    ) : (
+                        <CreateHabit habitArea="Humor" borderColor="#FE7F23" />
+                    )}
 
                 </View>
                 <Text
