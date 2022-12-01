81f2f4b2b656a2e04a4c295bfa4af453b7dc7563
Rafael Henrique da Silva Pereira <henrique014@gmail.com>
2022-12-01 14:28:25 -0300

    Estrutura das rotas e da HomePage

--- src/Routes/AllPages.jsx
+++ src/Routes/AllPages.jsx
@@ -18,6 +18,8 @@ export default function AllPages() {
                 <Stack.Screen name="Start" component={Start} />
                 <Stack.Screen name="AppExplanation" component={AppExplanation} />
                 <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="HabitPage" component={HabitPage} />
+
                 
             </Stack.Navigator>
         </NavigationContainer>
