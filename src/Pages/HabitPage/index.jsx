5cb72b272e59ed1370cc6c29ca58fb28e8707b5e
Rafael Henrique da Silva Pereira <henrique014@gmail.com>
2022-12-03 15:31:22 -0300

    Linkando o serviço com a pagina Update Habit

--- src/Pages/HabitPage/index.jsx
+++ src/Pages/HabitPage/index.jsx
@@ -81,13 +81,29 @@ export default function HabitPage({route}) {
     
     function handleUpdateHabit() {
         if (notificationToggle === true && !dayNotification && !timeNotification) {
-            Alert.alert("Você precisa colocar a frequência e horário da notificação");
-        } else {
-            navigation.navigate("Home", {
-                updatedHabit: `Updated in ${habit?.habitArea}`,
-            });
+                Alert.alert("Você precisa colocar a frequência e horário da notificação");
+            } else {
+                HabitsService.updateHabit({
+                    habitArea: habit?.habitArea,
+                    habitName: habitInput,
+                    habitFrequency: frequencyInput,
+                    habitHasNotification: notificationToggle,
+                    habitNotificationFrequency: dayNotification,
+                    habitNotificationTime: timeNotification,
+                    habitNotificationId: notificationToggle ? habitInput : null,
+                }).then(() => {
+                    Alert.alert("Sucesso na atualização do hábito");
+                    if (!notificationToggle) {
+                        //Delet
+                    } else {
+                        //Update
+                    }
+                    navigation.navigate("Home", {
+                        updatedHabit: `Updated in ${habit?.habitArea}`,
+                    });
+                });
+            }
         }
-    }
 
     return (
         <View style={styles.container}>
@@ -139,7 +155,7 @@ export default function HabitPage({route}) {
                         {create === false ? (
                             <UpdateExcludeButtons
                                 handleUpdate={handleUpdateHabit}
-                                habitArea={habitArea}
+                                habitArea={habit?.habitArea}
                                 habitInput={habitInput}
                             />
                         ) : (
