fdada22ff4d42476bf09e428910f6c8d70ce396e
Rafael Henrique da Silva Pereira <henrique014@gmail.com>
2022-12-03 13:26:56 -0300

    Redenização condicional de pagina e tempo de vida (No momento retornando NaN Verificar!)

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,27 +2,48 @@ import React, {useEffect, useState} from "react";
 import { useNavigation } from "@react-navigation/native";
 import {View, Text, StyleSheet, ScrollView} from "react-native";
 
+
+import CreateHabit from "../../Components/Home/CreateHabit";
 import LifeStatus from "../../Components/Common/LifeStatus";
 import StatusBar from "../../Components/Home/StatusBar";
-import CreateHabit from "../../Components/Home/CreateHabit/indes";
 import EditHabit from "../../Components/Home/EditHabit";
+import ChangeNavigationService from "../../service/ChangeNavigationService";
 
-export default function Home() {
+
+export default function Home({route}) {
     const navigation = useNavigation();
     const [mindHabit, setMindHabit] = useState();
     const [moneyHabit, setMoneyHabit] = useState();
     const [bodyHabit, setBodyHabit] = useState();
     const [funHabit, setFunHabit] = useState();
 
+    const [robotDaysLife, setRobotDaysLife] = useState();
+    const today = new Date();
+
     function handleNavExplanation() {
         navigation.navigate("AppExplanation");
     }
+
+    useEffect(() => {
+        ChangeNavigationService.checkShowHome(1)
+        .then((showHome) => {
+            const formDate = `${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`;
+            const checkDays =
+            // Por algum motivo retorna NaN Descobrir oq esta acontecendo
+                new Date(formDate) - new Date(showHome.appStartData) + 1;
+                setRobotDaysLife(checkDays.toString().padStart(2, "0"));
+            })
+        .catch((err) => console.log(err));
+    }, [route.params]);
+
+
 
     return (
         <View style={styles.container}>
             <ScrollView>
                 <View style={{alignItems:"center"}}>
-                    <Text style={styles.dailyChecks}>❤️ 20 dias - ✔️ 80 checks</Text>
+                    <Text style={styles.dailyChecks}>❤️ {robotDaysLife} {robotDaysLife === "01" ? "dia" : "dias"} - ✔️ 80 checks</Text>
+                    
 
                     <LifeStatus />
                     <StatusBar />
